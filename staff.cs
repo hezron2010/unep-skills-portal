4e2b7cb39f6c93f298d0751d37ca00d029758dc0
Hezron Peter <hezpeter@gmail.com>
2026-02-16 10:01:13 +0300

    Create staff.cs

---
+++ staff.cs
@@ -0,0 +1,15 @@
+public class Staff
+{
+    public int Id { get; set; }
+    public string IndexNumber { get; set; }
+    public string FullNames { get; set; }
+    public string Email { get; set; }
+    public string CurrentLocation { get; set; }
+    public int EducationLevelId { get; set; }
+    public int DutyStationId { get; set; }
+    public bool RemoteAvailability { get; set; }
+    public int SoftwareExpertiseId { get; set; }
+    public string SoftwareExpertiseLevel { get; set; }
+    public int LanguageId { get; set; }
+    public string LevelOfResponsibility { get; set; }
+}
